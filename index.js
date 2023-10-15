0d444ae417352741a4f3c3b157baffb15dce8f54
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-10-15 14:49:02 +0530

    using get method in expressjs

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-import express from 'express';
+import express, { response } from 'express';
+import path from 'path';
 
 const app = express();
+
+app.get("/", (req,res) => {
+
+    const pathlocation = path.resolve();
+
+    res.sendFile(path.join(pathlocation, "./index.html"));
+
+    /* res.json({
+        success: true,
+        products: [
+            'Yashvant Yadav',
+            'B.Tech',
+            'Computer Science'
+        ],
+    }); */
+});
 
 app.listen(5000, () => {
     console.log("app is running");
